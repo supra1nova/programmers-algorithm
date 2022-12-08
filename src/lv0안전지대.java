ab8058155ac43020f503f96cb9e27ab89981fd54
supra1nova <supra1nova@gmail.com>
2022-12-08 23:53:22 +0900

    2022-12-08 프로그래머스 lv0 알고리즘 문제 진행1

---
+++ src/lv0안전지대.java
@@ -0,0 +1,17 @@
+public class lv0안전지대 {
+    public static int solution1(int[][] board){
+        int[][] newBoard = new int[board.length][board.length];
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                if(board[i][j] == 1){
+                    int _i =
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
