0eb4748b71bbea27c5632ff925f5279db99b43c9
supra1nova <supra1nova@gmail.com>
2022-12-15 01:38:04 +0900

    2022-12-15 프로그래머스 lv0 알고리즘 문제 진행1

--- src/lv0안전지대.java
+++ src/lv0안전지대.java
@@ -1,17 +1,50 @@
 public class lv0안전지대 {
     public static int solution1(int[][] board){
-//        int[][] newBoard = new int[board.length][board.length];
-//        for(int i = 0; i < board.length; i++){
-//            for(int j = 0; j < board[i].length; j++){
-//                if(board[i][j] == 1){
-//                    int _i =
-//                }
-//            }
-//        }
-        return 0;
+        int[][] newBoard = new int[board.length][board.length];
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                if(board[i][j] == 1){
+                    newBoard[i][j] = 1;
+                    if(i - 1 >= 0){
+                        newBoard[i-1][j] = 1;
+                    }
+                    if(j - 1 >= 0){
+                        newBoard[i][j-1] = 1;
+                    }
+                    if(i - 1 >= 0 && j - 1 >= 0){
+                        newBoard[i-1][j-1] = 1;
+                    }
+                    if(i + 1 < board.length){
+                        newBoard[i+1][j] = 1;
+                    }
+                    if(j + 1 < board.length){
+                        newBoard[i][j+1] = 1;
+                    }
+                    if(i + 1 < board.length && j + 1 < board.length){
+                        newBoard[i+1][j+1] = 1;
+                    }
+                    if(i + 1 < board.length && j - 1 >= 0) {
+                        newBoard[i + 1][j - 1] = 1;
+                    }
+                    if(i - 1 >= 0 && j + 1 < board.length){
+                        newBoard[i-1][j+1] = 1;
+                    }
+                }
+            }
+        }
+        int answer = 0;
+        for(int i = 0; i < newBoard.length; i++){
+            for(int j = 0; j < newBoard.length; j++){
+                if(newBoard[i][j] == 0){
+                    answer++;
+                }
+            }
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
-
+        System.out.println("solution1(new int[][]{{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 0, 0}}) = " + solution1(new int[][]{{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 0, 0}}));
+        System.out.println("solution1(new int[][]{{1,1,1,1,1},{1,1,1,1,1},{1,1,1,1,1},{1,1,1,1,1},{1,1,1,1,1},}) = " + solution1(new int[][]{{1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1},}));
     }
 }
